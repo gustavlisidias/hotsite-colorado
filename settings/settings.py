d1251d86b7823445190865e5fbe82e549a7186e1
Gustavo Lisi Dias <118986616+gustavlisidias@users.noreply.github.com>
2022-11-24 11:31:37 -0300

    Update settings.py

--- settings/settings.py
+++ settings/settings.py
@@ -4,16 +4,16 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9f9^tbjrkm-p@wmvogv%m0&!84mzhs)ctdszhqm)trhplng!@7'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # Email sender configurations 
-EMAIL_HOST = 'notesserver.colorado.com.br'
-EMAIL_PORT = 25
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = False
+EMAIL_HOST = 
+EMAIL_PORT = 
+EMAIL_USE_TLS = 
+EMAIL_USE_SSL = 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # Application definition
@@ -127,9 +127,6 @@ USE_TZ = True
 ALLOWED_HOSTS = [
     'localhost', 
     '127.0.0.1',
-    '192.168.109.218',
-    '189.112.5.170',
-    'loja.coloradomaquinas.com.br'
 ]
 
 # Static and Media - dir and url config
